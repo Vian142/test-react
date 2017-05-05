f28339b058aa2d8cea7b7f0406b6dd5b1a1432bf
Ivan Popov <ivan.popov@quantron-systems.com>
2017-05-05 18:26:18 +0300

    Fix routes. add pages about and materials.

--- src/Routers.js
+++ src/Routers.js
@@ -3,13 +3,18 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './components/App';
 import MainPage from './components/pages/MainPage/MainPage';
 import TestPage from './components/pages/Tests/TestsPage/TestsPage';
+import About from './components/pages/About/About';
+import Materials from './components/pages/Materials/Materials';
 
-const Routers = <Router>
-    <App>
-        <Route path='/' component={MainPage} >
-            <Route path='tests' component={TestPage} />
-        </Route>
-    </App>
-</Router>;
+const Routers = () => (
+    <Router>
+        <App>
+            <Route path='/' exact={true} component={MainPage} />
+            <Route path='/tests' component={TestPage} />
+            <Route path='/about' component={About} />
+            <Route path='/materials' component={Materials} />
+        </App>
+    </Router>
+);
 
 export default Routers;
--- src/client.js
+++ src/client.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Routers from './Routers';
-import App from './components/App';
-import MainPage from './components/pages/MainPage/MainPage';
-import TestPage from './components/pages/Tests/TestsPage/TestsPage';
+//import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Routers from './Routers';
+// import App from './components/App';
+// import MainPage from './components/pages/MainPage/MainPage';
+// import TestPage from './components/pages/Tests/TestsPage/TestsPage';
 
 ReactDOM.render(
-  <Router>
-    <App>
-      <Route path='/' exact='/' component={MainPage} />
-      <Route path='/tests' component={TestPage} />
-    </App>
-  </Router>,
+  <Routers/>,
   document.getElementById('root'),
 );
 
