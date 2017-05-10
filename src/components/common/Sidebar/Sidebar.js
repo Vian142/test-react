50c942886f4b7a74089f5f481bcbacc24047c2c1
Vian142 <vian142@gmail.com>
2017-05-11 01:33:40 +0300

    add sidebar list. need fix.

--- src/components/common/Sidebar/Sidebar.js
+++ src/components/common/Sidebar/Sidebar.js
@@ -7,7 +7,47 @@ import styles from './styles.css';
 // /////////////////////////////////////////////////////////////////////////////
 function Sidebar() {
     return <div className={styles.sidebarWrapper}>
-        Сайдбар
+        <div className={styles.sidebarItem}>
+            <div className={styles.sidebarItemTitle}>Категории</div>
+            <ul className={styles.sidebarList}>
+                <li className={styles.sidebarListItem}>
+                    <input type="checkbox" id="item1" />
+                    <label className={styles.listLabelTitle} htmlFor="item1">Пункт 1 уровня </label>
+                    <ul className={styles.dropdown}>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                    </ul>
+                </li>
+                <li className={styles.sidebarListItem}>
+                    <input type="checkbox" id="item2" />
+                    <label className={styles.listLabelTitle} htmlFor="item2">Пункт 1 уровня </label>
+                    <ul className={styles.dropdown}>
+                        <li><a href="#">Подпункт 1 уровня и оооооочень длиннный блин</a></li>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                    </ul>
+                </li>
+                <li className={styles.sidebarListItem}>
+                    <input type="checkbox" id="item3" />
+                    <label className={styles.listLabelTitle} htmlFor="item3">Пункт 1 уровня </label>
+                    <ul className={styles.dropdown}>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                    </ul>
+                </li>
+                <li className={styles.sidebarListItem}>
+                    <input type="checkbox" id="item4" />
+                    <label className={styles.listLabelTitle} htmlFor="item4">Пункт 1 уровня </label>
+                    <ul className={styles.dropdown}>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                        <li><a href="#">Подпункт 1 уровня</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>;
 }
 
