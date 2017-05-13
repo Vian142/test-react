b1f441c9e82697537de5cffdfd7265f757e337f2
Vian142 <vian142@gmail.com>
2017-05-13 21:37:44 +0300

    style for testCard. fix style sidebar.

--- src/components/common/Sidebar/Sidebar.js
+++ src/components/common/Sidebar/Sidebar.js
@@ -12,29 +12,38 @@ function Sidebar() {
             <ul className={styles.sidebarList}>
                 <li className={styles.sidebarListItem}>
                     <input type="checkbox" id="item1" />
-                    <label className={styles.listLabelTitle} htmlFor="item1">Пункт 1 уровня </label>
+                    <label className={styles.listLabelTitle} htmlFor="item1">Иностранные языки</label>
                     <ul className={styles.dropdown}>
-                        <li><a href="#">Подпункт 1 уровня</a></li>
-                        <li><a href="#">Подпункт 1 уровня</a></li>
-                        <li><a href="#">Подпункт 1 уровня</a></li>
+                        <li><a href="#">Английский язык</a></li>
+                        <li><a href="#">Немецкий язык</a></li>
+                        <li><a href="#">Французский язык</a></li>
+                        <li><a href="/">Португальский</a></li>
                     </ul>
                 </li>
                 <li className={styles.sidebarListItem}>
                     <input type="checkbox" id="item2" />
-                    <label className={styles.listLabelTitle} htmlFor="item2">Пункт 1 уровня </label>
+                    <label className={styles.listLabelTitle} htmlFor="item2">История</label>
                     <ul className={styles.dropdown}>
-                        <li><a href="#">Подпункт 1 уровня и оооооочень длиннный блин</a></li>
-                        <li><a href="#">Подпункт 1 уровня</a></li>
-                        <li><a href="#">Подпункт 1 уровня</a></li>
+                        <li><a href="#">Основы археологии</a></li>
+                        <li><a href="#">Основы этнологии</a></li>
+                        <li><a href="#">История первобытного общества</a></li>
+                        <li><a href="#">история России до начала XIX</a></li>
+                        <li><a href="#">История нового времени стран Европы и
+                            Америки (середина XVII в. - 1918 г.)</a></li>
+                        <li><a href="#">стран Азии и Африки в новое и новейшее время</a></li>
                     </ul>
                 </li>
                 <li className={styles.sidebarListItem}>
                     <input type="checkbox" id="item3" />
-                    <label className={styles.listLabelTitle} htmlFor="item3">Пункт 1 уровня </label>
+                    <label className={styles.listLabelTitle} htmlFor="item3">Экономика</label>
                     <ul className={styles.dropdown}>
-                        <li><a href="#">Подпункт 1 уровня</a></li>
-                        <li><a href="#">Подпункт 1 уровня</a></li>
-                        <li><a href="#">Подпункт 1 уровня</a></li>
+                        <li><a href="#">Антикризисное управление</a></li>
+                        <li><a href="#">Бухгалтерский учет и анализ</a></li>
+                        <li><a href="#">Деньги, кредит, банки</a></li>
+                        <li><a href="#">Институциональная экономика</a></li>
+                        <li><a href="#">Экономика и организация инновационной деятельности</a></li>
+                        <li><a href="#">Экономика качества</a></li>
+                        <li><a href="/">Экономика организаций (предприятия)</a></li>
                     </ul>
                 </li>
                 <li className={styles.sidebarListItem}>
--- src/components/pages/Tests/TestCard/TestCard.js
+++ src/components/pages/Tests/TestCard/TestCard.js
@@ -7,7 +7,7 @@ import styles from './styles.css'
 ///////////////////////////////////////////////////////////////////////////////
 function getLevel(level) {
     const stars = _.times(
-        level, () => <i className={classnames('fa', 'fa-star')} />
+        5, (index) => <i className={classnames('fa fa-star', {[styles.iconOrange]: (level > index)})} />
     );
     return stars;
 }
@@ -15,28 +15,33 @@ function getLevel(level) {
 ///////////////////////////////////////////////////////////////////////////////
 function TestCard(props) {
     const { data } = props;
-    return <div className={styles.wrapperCard}>
-        <div className={styles.cardTitle}>{data.title}</div>
-        <div className={styles.cardDescription}>{data.description}</div>
-        <div className={styles.cardInfo}>
-            <div className={styles.cardCategory}>
-                Категория: <a href='#' className={styles.categoryLink}>
-                    {data.category}
-                </a>
+    return (
+        <div key={data.id} className={styles.wrapperCard}>
+            <div className={styles.cardTop}>
+                <div title='Номер теста' className={styles.cardTopId}>#{data.id}</div>
+                <div className={styles.cardTopLevel}>
+                    <span title='Сложность' className={styles.testLevel}>
+                        {getLevel(data.level)}
+                    </span>
+                </div>
             </div>
-            <div className={styles.level}>
-                Сложность: {
-                    getLevel(data.level)
-                }
+            <div className={styles.cardTitle}>{data.title}</div>
+            <div className={styles.cardDescription}>{data.description}</div>
+            <div className={styles.cardInfo}>
+                <div className={styles.cardCategory}>
+                    Категория: <a href='#' className={styles.categoryLink}>
+                        {data.category}
+                    </a>
+                </div>
+            </div>
+            <div className={styles.btnWrapper}>
+                <LinkButton
+                    styleClass={styles.cardBtn}
+                    type='link'
+                    url='/'>Начать тест</LinkButton>
             </div>
         </div>
-        <div className={styles.btnWrapper}>
-            <LinkButton
-                styleClass={styles.cardBtn}    
-                type='link'
-                url='/'>Перейти</LinkButton>
-        </div>
-    </div>;
+    );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
