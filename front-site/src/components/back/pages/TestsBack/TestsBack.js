1813d30a058e2f017e8c3aac85322b60c2330e12
Ivan Popov <ivan.popov@quantron-systems.com>
2017-05-16 18:41:36 +0300

    add style for table

--- front-site/src/components/back/pages/TestsBack/TestsBack.js
+++ front-site/src/components/back/pages/TestsBack/TestsBack.js
@@ -1,20 +1,62 @@
 ///////////////////////////////////////////////////////////////////////////////
 import React from 'react';
+import _ from 'lodash';
+import classnames from 'classnames';
 import StaticContent from '../../common/StaticContent/StaticContent';
 import styles from './styles.css';
 
 ///////////////////////////////////////////////////////////////////////////////
 const propsPage = {
-    titlePage: 'Страница'
+    titlePage: 'Страница тестов'
 }
+
+///////////////////////////////////////////////////////////////////////////////
+function Controls() {
+    return <div>
+        <span
+            title='Подробнее'
+            className={classnames(styles.btnControl, 'fa fa-eye')} />
+        <span
+            title='Отправить результаты на Email'
+            className={classnames(styles.btnControl, 'fa fa-envelope-o')} />
+    </div>;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+function TableRow() {
+    return <tr className={styles.tableItem}>
+        <td className={styles.tableCell}>Основы экономики и предприятия</td>
+        <td className={styles.tableCell}>Экономика</td>
+        <td className={styles.tableCell}>7/10</td>
+        <td className={styles.tableCellDate}>25.04.2017</td>
+        <td className={classnames(styles.tableCellControl)}>
+            <Controls />
+        </td>
+    </tr>
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////
 function TestsBack() {
     return <StaticContent {...propsPage}>
         <div className={styles.mainWrapper}>
-            <div className={styles.content}>
-                Таблица
-            </div>
+            <div className={styles.title}>Последние тесты</div>
+            <table className={styles.tableContainer}>
+                <thead className={styles.tableHead}>
+                    <tr>
+                        <th>Название теста</th>
+                        <th>Категория</th>
+                        <th>Результат</th>
+                        <th>Дата</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        _.times(9, (index) => <TableRow key={index} />)
+                    }
+                </tbody>
+            </table>
         </div>
     </StaticContent>;
 }
