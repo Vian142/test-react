daef6c2b1adeb3a7b13fec088f9f4bcff3a4faed
Vian142 <vian142@gmail.com>
2017-05-17 00:22:47 +0300

    add recharts for statistics data.

--- front-site/src/components/back/pages/StatisticsPage/StatisticsPage.js
+++ front-site/src/components/back/pages/StatisticsPage/StatisticsPage.js
@@ -1,5 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 import React from 'react';
+import { PieChart, Pie, Cell } from 'recharts';
 import StaticContent from '../../common/StaticContent/StaticContent';
 import ItemContent from '../../common/ItemContent/ItemContent';
 import styles from './styles.css';
@@ -8,6 +9,12 @@ import styles from './styles.css';
 const propsPage = {
     titlePage: 'Страница статистики'
 }
+
+const data = [
+    { name: 'Group A', value: 32 },
+    { name: 'Group B', value: 10 }];
+const colors = ['#1cc331', '#DD1C1A'];
+
 
 ///////////////////////////////////////////////////////////////////////////////
 function StatisticsPage() {
@@ -19,7 +26,15 @@ function StatisticsPage() {
                         Блок статистики
                     </div>
                     <div className={styles.itemContent}>
-                        
+                        <PieChart width={240} height={300}>
+                            <Pie data={data} cx="50%" cy="50%" outerRadius={80} label>
+                                {
+                                    data.map((entry, index) => (
+                                        <Cell key={`cell-${index}`} fill={colors[index]} />
+                                    ))
+                                }
+                            </Pie>
+                        </PieChart>
                     </div>
                 </ItemContent>
             </div>
