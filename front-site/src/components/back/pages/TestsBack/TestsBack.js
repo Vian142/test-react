924b65e724ab6c3fd729b2108300841a68cf51be
Ivan Popov <ivan.popov@quantron-systems.com>
2017-05-29 17:44:17 +0300

    fix style for table last tests in admin

--- front-site/src/components/back/pages/TestsBack/TestsBack.js
+++ front-site/src/components/back/pages/TestsBack/TestsBack.js
@@ -1,21 +1,26 @@
 ///////////////////////////////////////////////////////////////////////////////
 import React from 'react';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 import classnames from 'classnames';
+import LinkButton from '../../common/LinkButton/LinkButton';
 import StaticContent from '../../common/StaticContent/StaticContent';
 import styles from './styles.css';
 
 ///////////////////////////////////////////////////////////////////////////////
 const propsPage = {
-    titlePage: 'Страница тестов'
+    titlePage: 'Страница последних тестов'
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 function Controls() {
     return <div>
-        <span
+        <LinkButton
             title='Подробнее'
-            className={classnames(styles.btnControl, 'fa fa-eye')} />
+            icon='fa-eye'
+            url='/admin/test/1'
+            classStyle={classnames(styles.btnControl)}
+        />
         <span
             title='Отправить результаты на Email'
             className={classnames(styles.btnControl, 'fa fa-envelope-o')} />
@@ -25,7 +30,12 @@ function Controls() {
 ///////////////////////////////////////////////////////////////////////////////
 function TableRow() {
     return <tr className={styles.tableItem}>
-        <td className={styles.tableCell}>Основы экономики и предприятия</td>
+        <td className={styles.tableCell}>
+            <Link
+                to='/admin/tests'
+                title='Подробнее'
+                className={styles.tableItemLink}>Основы экономики и предприятия</Link>
+        </td>
         <td className={styles.tableCell}>Экономика</td>
         <td className={styles.tableCell}>7/10</td>
         <td className={styles.tableCellDate}>25.04.2017</td>
@@ -40,7 +50,15 @@ function TableRow() {
 function TestsBack() {
     return <StaticContent {...propsPage}>
         <div className={styles.mainWrapper}>
-            <div className={styles.title}>Последние тесты</div>
+            <div className={styles.btnWrapper}>
+                <LinkButton
+                    title='На главную'
+                    icon='fa-reply'
+                    url='/admin/'
+                    styleClass={classnames(styles.btn, styles.btnBlue)}>
+                    На главную
+                </LinkButton>
+            </div>
             <table className={styles.tableContainer}>
                 <thead className={styles.tableHead}>
                     <tr>
